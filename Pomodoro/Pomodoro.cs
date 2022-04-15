a09c3868ef75fa498f87aec1e0ac09e3efa318ee
Gustavo Silva <93677342+ZetteCynosure@users.noreply.github.com>
2022-04-15 15:19:14 +0000

    Initial commit

---
+++ Pomodoro/Pomodoro.cs
@@ -0,0 +1,39 @@
+﻿namespace Pomodoro
+{
+	public class PomodoroTimer
+	{
+		public static void Timer(int Minutes)
+		{
+			int Seconds = 1;
+			
+			while(Minutes >= 0 && Seconds > 0)
+			{
+				Seconds--;
+				if (Seconds == 0)
+				{
+					Minutes--;
+					Seconds += 59;
+
+					if (Minutes == -1)
+						break;
+				}
+				
+				string Time = ( Minutes < 10 && Seconds < 10 )? $"0{Minutes}:0{Seconds}" : ( Minutes < 10 )? $"0{Minutes}:{Seconds}" : $"{Minutes}:{Seconds}";
+
+				Console.Clear();
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine($@"	Pomodoro Timer
+
+	.---------.
+	|  {Time}  |
+	.---------.
+	");
+			Thread.Sleep(1000);
+			}
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("\n\tPomodoro has stoped!\n");
+			Console.ForegroundColor = ConsoleColor.White;
+		}
+	}
+}
---
+++ Tasks/Task.cs
@@ -0,0 +1,82 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Tasks
+{
+	public class _Task
+	{
+		public int Id { get; set; }
+		public string Name { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public DateTime Date { get; set; }
+		public bool Done { get; set; }
+
+		private static int TempId = 1;
+		// Using CsvHelper to add a row in the task.csv file.
+
+		public static void AddTask(string name, string desc, DateTime date, bool done = false)
+		{
+			var Records = new List<_Task>
+			{
+				new _Task {Id = TempId, Name = name, Description = desc, Date = date, Done = done}
+			};
+				
+			// If you want to modify the path, make sure that is correctly typed and the folder contains a csv file.
+
+			using (var Writer = new StreamWriter(@"../ProductivityApp/task.csv"))
+			using (var Csv = new CsvWriter(Writer, CultureInfo.InvariantCulture))
+			{
+				Csv.WriteRecords(Records);
+			}
+
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Task added succesfully!");
+			Console.ResetColor();
+
+			TempId++;
+		}
+
+		// In this method, we pull all of the records into memory and then write 'em back to the file.
+
+		public static void DeleteTask(int RemoveId)
+		{
+			List<_Task> Records;
+
+			using (var Reader = new StreamReader(@"../ProductivityApp/task.csv"))
+			using (var Csv = new CsvReader(Reader, CultureInfo.InvariantCulture))
+			{
+				Records = Csv.GetRecords<_Task>().ToList();
+
+				for(int i = 0 ; i < Records.Count ; ++i)
+				{
+					if( Records[i].Id ==  RemoveId)
+					{
+						Records.RemoveAt(i);
+					}
+				}
+			}
+
+			using (var Writer = new StreamWriter(@"../ProductivityApp/task.csv"))
+			using (var CsvWriter = new CsvWriter(Writer, CultureInfo.InvariantCulture))
+			{
+				CsvWriter.WriteRecords(Records);
+			}
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine("Your task was deleted succesfully.");
+			Console.ResetColor();
+		}
+		
+		public static void ReadTask()
+		{ 
+			string[] AllTasks = File.ReadAllLines(@"../ProductivityApp/task.csv");
+			Console.WriteLine("Your current tasks are: ");
+			foreach(string Line in AllTasks)
+			{
+				if(Line != "Id,Name,Description,Date,Done")
+					Console.WriteLine($"\t{Line}");
+			}
+		}
+	}
+}
