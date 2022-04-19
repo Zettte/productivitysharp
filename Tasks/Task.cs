70976a8306f4dc1523eb81b710a08aacd02db6de
Gustavo Silva <93677342+ZetteCynosure@users.noreply.github.com>
2022-04-19 09:31:01 -0400

    Fixed "adding task" bug

--- Tasks/Task.cs
+++ Tasks/Task.cs
@@ -10,44 +10,44 @@ namespace Tasks
 			string done = ( Done == false ) ? "Not done yet" : "Done";
 			string TaskInfo = Name + ", " + Description + ", " + Date.ToString() + ", " + done; 
 
-			using StreamWriter File = new ($@"..{Path.DirectorySeparatorChar}ProductivityApp{Path.DirectorySeparatorChar}tasks.txt", append:true);
+			using StreamWriter File = new ($".{Path.DirectorySeparatorChar}tasks.txt", append:true);
 			File.WriteLine(TaskInfo);
 
 			Console.Clear();
 			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Task added succesfully!");
+			Console.WriteLine("\t\t\t\tTask added succesfully!");
 			Console.ResetColor();
 		}
 
 		public static void DeleteAllTasks()
 		{
-			File.Delete($@"..{Path.DirectorySeparatorChar}ProductivityApp{Path.DirectorySeparatorChar}tasks.txt");
+			File.Delete($".{Path.DirectorySeparatorChar}tasks.txt");
 
 			Console.Clear();
 			Console.ForegroundColor = ConsoleColor.Red;
-			Console.WriteLine("Your tasks were deleted succesfully.");
+			Console.WriteLine("\t\t\t\tYour tasks were deleted succesfully.");
 			Console.ResetColor();
 
-			File.Create($@"..{Path.DirectorySeparatorChar}ProductivityApp{Path.DirectorySeparatorChar}tasks.txt");
+			File.Create($".{Path.DirectorySeparatorChar}tasks.txt");
 		}
 		
 		public static void ReadTask()
 		{ 
-			if( new FileInfo($@"..{Path.DirectorySeparatorChar}ProductivityApp{Path.DirectorySeparatorChar}tasks.txt").Length == 0)
+			if( new FileInfo($".{Path.DirectorySeparatorChar}tasks.txt").Length == 0)
 			{
 				Console.ForegroundColor = ConsoleColor.Red;
-				Console.WriteLine("Please add some tasks.");
+				Console.WriteLine("\t\t\t\tPlease add some tasks.");
 				Console.ResetColor();
 			}
 			else
 			{
-				string[] AllTasks = File.ReadAllLines($@"..{Path.DirectorySeparatorChar}ProductivityApp{Path.DirectorySeparatorChar}tasks.txt");
+				string[] AllTasks = File.ReadAllLines($".{Path.DirectorySeparatorChar}tasks.txt");
 			
 				Console.Clear();
-				Console.WriteLine("Your current tasks are: ");
+				Console.WriteLine("\t\t\t\tYour current tasks are: ");
 				foreach(string Line in AllTasks)
 				{
-					Console.WriteLine($"\t{Line}");
+					Console.WriteLine($"\t\t\t\t\t{Line}");
 				}
 				Console.WriteLine();
 			}
